f0f679c48c1f61442b25d496b4309e8a038e64ed
Clément Escolano <clement.escolano@icloud.com>
2017-11-12 19:39:50 +0100

    Add shuffle method

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ import Button from './Button';
 
 const apiToken = '<<Copiez le token de Spotify ici>>';
 
+function shuffleArray(array) {
+  let counter = array.length;
+
+  while (counter > 0) {
+    let index = getRandomNumber(counter);
+    counter--;
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
 /* Return a random number between 0 included and x excluded */
 function getRandomNumber(x) {
   return Math.floor(Math.random() * x);

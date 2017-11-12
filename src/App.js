d5339f583b9df00796803d25d6a9464b3ac7eed7
Clément Escolano <clement.escolano@icloud.com>
2017-11-12 11:27:37 +0100

    Empty project ready for Blindtest demo

--- src/App.js
+++ src/App.js
@@ -8,11 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to your Blindtest</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </main>
       </div>
     );
   }

b6af718b47608b651dd85bf475fe60c4dfc7c5c4
MathildeDuboille <duboille.m@gmail.com>
2020-02-22 16:04:19 +0100

    solution backlog 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 /*global swal*/
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import loading from './loading.svg';
 import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
+import AlbumCover from "./AlbumCover";
 
 const apiToken = '<<Copiez le token de Spotify ici>>';
 
@@ -29,6 +30,34 @@ function getRandomNumber(x) {
 }
 
 const App = () => {
+  const [tracks, setTracks] = useState();
+  const [songsLoaded, setSongsLoaded] = useState(false);
+  useEffect(() => {
+    fetch('https://api.spotify.com/v1/playlists/1wCB2uVwBCIbJA9rar5B77/tracks', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + apiToken,
+      },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        setTracks(data.items);
+        setSongsLoaded(true);
+      });
+  }, []);
+
+  if (!songsLoaded) {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo"/>
+      </div>
+    );
+  }
+
+  const track1 = tracks[0].track;
+  const track2 = tracks[1].track;
+  const track3 = tracks[2].track;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -36,9 +65,13 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le Blindtest</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+        <AlbumCover track={track1}/>
+        <Sound url={track1.preview_url} playStatus={Sound.status.PLAYING}/>
       </div>
       <div className="App-buttons">
+        <Button>{track1.name}</Button>
+        <Button>{track2.name}</Button>
+        <Button>{track3.name}</Button>
       </div>
     </div>
   );
